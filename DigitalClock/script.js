db60bee76d1716c3127a79426d38e68e65d4af7b
Venkatesh <venkat@print2block.com>
2025-01-05 13:52:17 +0530

    digital clock work completed

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -3,7 +3,7 @@ const hour = document.querySelector(".hour")
 const min = document.querySelector(".min")
 const sec = document.querySelector(".sec")
 
-setClock = () => {
+const setClock = () => {
   let day = new Date()
   let hh = day.getHours() * 30
   let mm = day.getMinutes() * deg
@@ -12,6 +12,16 @@ setClock = () => {
   hour.style.transform = `rotateZ(${hh + mm / 12}deg)`
   min.style.transform = `rotateZ(${mm}deg)`
   sec.style.transform = `rotateZ(${ss}deg)`
+
+  let hours = String(day.getHours()).padStart(2, "0")
+  let minutes = String(day.getMinutes()).padStart(2, "0")
+  let seconds = String(day.getSeconds()).padStart(2, "0")
+
+  document.getElementById("hours").textContent = hours
+  document.getElementById("minutes").textContent = minutes
+  document.getElementById("seconds").textContent = seconds
 }
 
 setInterval(setClock, 1000)
+
+setClock()
